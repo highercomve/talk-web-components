a5fc09e4ba0e1a88b3a61c5cb8e98526b60e6b70
Sergio Marin <higher.vnf@gmail.com>
2018-07-05 21:34:34 -0400

    Start giphy

---
+++ examples/004-giphy-search/index.js
@@ -0,0 +1,32 @@
+export default class GiphyApi {
+  constructor(apiKey) {
+    if (!apiKey) {
+      throw new Error('apiKey must be provided.');
+    }
+
+    this.apiKey = apiKey;
+  }
+
+  async search(search, limit = 10, offset = 0, rating = 'G', lang = 'en') {
+    if (!search) {
+      throw new Error('search must be provided.');
+    }
+
+    const url = 'https://api.giphy.com/v1/gifs/search?'
+      + `api_key=${this.apiKey}`
+      + `&q=${search}`
+      + `&limit=${limit}`
+      + `&offset=${offset}`
+      + `&rating=${rating}`
+      + `&lang=${lang}`;
+
+    const response = await fetch(url);
+    const json = await response.json();
+
+    return json;
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  CreateComponent('giphy-search', {})
+})
